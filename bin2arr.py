98babc15a097451d277b956ee1b35bd8294ae5b7
CHWu <tarsiertail@gmail.com>
2018-08-14 19:37:38 +0800

    read raw file

--- bin2arr.py
+++ bin2arr.py
@@ -3,14 +3,21 @@ Created on Tue Aug 14 17:58:44 2018
 @author: chwu
 """
 
+import matplotlib.pyplot as plt
+
+
 def read_data(filename):
     with open(filename, 'rb') as infile:
         lines = []
+        infile.seek(512)
         byte = infile.read(1)
         lines.append(byte)
         while byte:
         # Do stuff with byte.
             byte = infile.read(1)
-            lines.append(byte)
+            lines.append(int.from_bytes(byte,'little'))
     infile.close
     return lines
+
+plt.plot(read_data('testfile.RAW'))
+plt.show()
